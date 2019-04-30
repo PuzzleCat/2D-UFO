82b93cb96c26695d2b058080d712c5b9ddf22b45
PuzzleCat <48221543+PuzzleCat@users.noreply.github.com>
2019-04-30 01:50:42 -0400

    Add files via upload

---
+++ AlienController.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class AlienController : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody2D rb;
+	private Vector2 movement;
+
+	void Start(){
+		rb = GetComponent<Rigidbody2D> ();
+
+		float h = Random.value;
+		float v = Random.value;
+
+		float moveHorizontal = h * 2 - 1;
+		float moveVertical = v * 2 - 1;
+
+		movement = new Vector2 (moveHorizontal, moveVertical);
+
+		rb.velocity = movement * speed;
+	}
+
+	void OnCollisionEnter2D(Collision2D other){
+		if (other.gameObject.CompareTag ("Wall")) {
+			float Horizontal = Random.value * 2 - 1;
+			float Vertical = Random.value * 2 - 1;
+			movement = new Vector2 (Horizontal, Vertical);
+			rb.velocity = movement * speed;
+		} 
+		else if (other.gameObject.CompareTag ("Alien")) {
+			float Horizontal = Random.value * 2 - 1;
+			float Vertical = Random.value * 2 - 1;
+			movement = new Vector2 (Horizontal, Vertical);
+			rb.velocity = movement * speed;
+		}
+		else if (other.gameObject.CompareTag ("BlackAlien")) {
+			float Horizontal = Random.value * 2 - 1;
+			float Vertical = Random.value * 2 - 1;
+			movement = new Vector2 (Horizontal, Vertical);
+			rb.velocity = movement * speed;
+		}
+	}
+
+	/**public float speed;
+	public Vector2 newDir;
+	
+	private Rigidbody2D rb;
+	private Vector2 movement;
+	
+	void Start(){
+		rb = GetComponent<Rigidbody2D>();
+
+		float h = Random.value;
+		float v = Random.value;
+
+		float moveHorizontal = h * 2 - 1;
+		float moveVertical = v * 2 - 1;
+		
+		movement = new Vector2 (moveHorizontal, moveVertical);
+
+		move ();
+	}
+
+	void move(){
+		rb.AddForce (movement * speed);
+	}
+
+	void OnCollisionEnter2D(Collider2D other){
+			
+		movement = new Vector2 (Random.value*2-1, Random.value*2-1);
+
+		move ();
+	}
+
+	void Update(){
+		move ();
+	}
+**/
+}
---
+++ PlayerController.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	
+	public float speed;
+	public GameObject flare;
+	public GameObject blackFlare;
+	public Text alienCount;
+	public Text done;
+	
+	private Rigidbody2D rb;
+	private bool cont;
+	private int aCount;
+	
+	void Start(){
+		rb = GetComponent<Rigidbody2D>();
+		cont = true;
+		aCount = 0;
+		alienCount.text = "Count: 0";
+		done.text = "";
+	}
+	
+	void FixedUpdate(){
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+		
+		rb.AddForce (movement*speed);
+	}
+
+	void OnCollisionEnter2D(Collision2D other){
+		if (other.gameObject.CompareTag ("Alien")) {
+			if(cont==true){
+				flare.SetActive(true);
+				aCount = aCount + 1;
+				alienCount.text = "Count: " + aCount.ToString();
+				other.gameObject.SetActive (false);
+				StartCoroutine(delay());
+				if(aCount==24){
+					cont = false;
+				}
+			}
+		}
+		if (other.gameObject.CompareTag ("BlackAlien")) {
+			blackFlare.SetActive(true);
+			other.gameObject.SetActive (false);
+			StartCoroutine(delay());
+			if (aCount < 24 && cont == true) {
+				cont = false;
+				done.text = "You Lose!";
+			} else {
+				done.text = "You Win!";
+			}
+		}
+	}
+
+	IEnumerator delay(){
+		yield return new WaitForSeconds (1);
+		flareOff ();
+	}
+
+	void flareOff(){
+		flare.SetActive (false);
+		blackFlare.SetActive (false);
+	}
+
+}
